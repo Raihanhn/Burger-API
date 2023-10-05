5d7d15f005207f85ab3368cdfa5e98a1398aaeb2
Raihan <raihanhn20@gmail.com>
2023-10-06 05:09:39 +0600

    new way

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+// server.use(router);
 
-server.listen(port);
+// server.listen(port);
+
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
